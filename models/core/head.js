72c89eac5c533c2f53f063b302fa6c6e93042694
Rudda Beltrão <beltrao.rudah@gmail.com>
2019-08-28 00:06:09 -0400

    feat: init structure project
    A dependecy tree was defined with core in Programando NCL3.0 Book p.122

---
+++ models/core/head.js
@@ -0,0 +1,12 @@
+/**Body must to follow a order
+ *     (recomendation only)
+ * 1 - importedDocumentBase
+ * 2 - ruleBase
+ * 3-  transitionBase
+ * 4 - RegionBase
+ * 5 - DescriptorBase
+ * 6 - ConnectorBase
+ * 7 - meta
+ * 8 - metadata
+ * 
+ */
---
+++ models/core/ncl.js
@@ -0,0 +1,16 @@
+/**
+ * create by Rudda Beltrão on Ago 27th - 2019
+ * 
+ */
+
+const Body = require('./body');
+const Head = require('./head');
+
+const Ncl = {
+
+    body: Body,
+    head: Head
+
+};
+
+module.exports = Ncl;
