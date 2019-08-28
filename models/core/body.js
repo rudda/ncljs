f3db8e87d194b84629e741ab9bb6007c3b92c075
Rudda Beltrão <beltrao.rudah@gmail.com>
2019-08-28 00:25:20 -0400

    feat: init body structure was defined

---
+++ models/core/body.js
@@ -0,0 +1,15 @@
+/**
+ *  Body created by Rudda Beltrao
+ * 
+ */
+
+ const Body = {
+    port : Port,
+    property : Property,
+    media : Media,
+    context : Context,
+    switch: Switch,
+    link: Link,
+    meta: Meta,
+    metadata: MetaData
+ };
