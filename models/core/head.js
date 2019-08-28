5245e6e0dff9ee47cf2233852ae03fda82c7f6bb
Rudda Beltrão <beltrao.rudah@gmail.com>
2019-08-28 00:19:30 -0400

    feat: set initial structure for head

--- models/core/head.js
+++ models/core/head.js
@@ -1,4 +1,7 @@
-/**Body must to follow a order
+/**
+ * create by Rudda Beltrao
+ * 
+ * Body must to follow a order
  *     (recomendation only)
  * 1 - importedDocumentBase
  * 2 - ruleBase
@@ -10,3 +13,25 @@
  * 8 - metadata
  * 
  */
+
+ const ImportedDocumentBase = require('../modules/import/imported_document_base');
+ const RuleBase = require('../modules/test_rule/RuleBase');
+ const TransitionBase = require('../modules/transition_base/transition_base');
+ const RegionBase = require('../modules/layout/region_base');
+ const DescriptorBase = require('../modules/descriptor/descriptor_base');
+ const ConnectorBase = require('../modules/connector_base/connector_base');
+ const Meta = require('../modules/meta_information/meta');
+ const MetaData = require('../modules/meta_information/metadata');
+
+ const Head = {
+    importedDocumentBase : ImportedDocumentBase,
+    ruleBase : RuleBase,
+    transitionBase : TransitionBase,
+    regionBase : RegionBase,
+    descriptorBase : DescriptorBase,
+    connectorBase : ConnectorBase,
+    meta: Meta,
+    metaData : MetaData       
+ };
+
+ module.exports = Head;
