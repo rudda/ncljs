22f5cbb38380be36186f38ec47d3de69f069e409
Rudda Beltrão <beltrao.rudah@gmail.com>
2020-01-07 23:27:00 -0400

    chore: 1st test with custom html tags

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log(document.getElementsByTagName("rudda"));
+function init() {
+
+  document.createElement("ncl-media")
+  var medias = document.getElementsByTagName("ncl-media")
+  var x = document.getElementsByTagName("body");
+  console.log(medias[0].attributes)
+  
+}
+
+window.onload = init
