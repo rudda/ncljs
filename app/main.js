0980f778305d715565c0962f2ca5147efe9b0ff8
Ruddá Beltrão <beltrao.rudah@gmail.com>
2020-01-29 08:23:16 -0400

    chore: change ncl model structure

--- app/main.js
+++ app/main.js
@@ -2,9 +2,5 @@ const xmlbuilder = require('xmlbuilder');
 
 var Ncl = require('./model/ncl');
 var a = new Ncl();
-a.id ="ncl";
-a.xml.ele("oi");
 
-person = xmlbuilder.create('person').att('id', 1);
-a.xml.importDocument(person);
-console.log(a.xml.end({pretty:true}));
+console.log(a.root.end({pretty:true}));
--- app/model/ncl.js
+++ app/model/ncl.js
@@ -3,50 +3,19 @@
 // MIT license
 
 const NCLNS_TYPE = require('./types/nclns.types');
-const builder = require('xmlbuilder');
+const CommonNode = require('./common/common.node');
 
-class Ncl {
+class Ncl extends CommonNode {
 
     /** set default value of xmlns attribuite */
     constructor() {
-        
+
+        super('ncl', [{ version: '1.0'}, {encoding: 'UTF-8' }]);
         this._xmlns = NCLNS_TYPE.XMLNS_BDTV;
         this._encoding = 'UTF-8';
-        this._xml = builder.create('ncl',  { version: '1.0', encoding: this.encoding } );
-    }
 
-    set id(id) {
-        this._id = id;
-    }
-
-    get id() {
-        return this._id;
-    }
-
-    set xmlns(xmlns) {
-        this._xmlns = xmlns;
-    }
-
-    get xmlns() {
-        return this._xmlns;
-    }
-
-    set title(title) {
-        this._title = title;
-    }
-
-    get title() {
-        return this._title;
-    }
-
-    get xml() {
-        return this._xml;
     }
     
-    set xml( xml ) {
-        //TODO ??
-    }
-
 }
 
 module.exports = Ncl;
